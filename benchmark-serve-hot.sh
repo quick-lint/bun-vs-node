5738d0668f3b10638b1ea8147e803bddd4418d1c
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-09-12 02:50:06 -0700

    Improve dev server benchmarks

--- benchmark-serve-hot.sh
+++ benchmark-serve-hot.sh
@@ -3,9 +3,20 @@ set -e
 set -u
 set -o pipefail
 
+(cd website-node-20 && yarn install)
+(cd website-bunnode && yarn install)
+(cd website-bunbuns && bun install)
+
 hyperfine \
   --export-json=benchmark-serve-hot.json \
   --warmup=1 \
   --time-unit=millisecond \
-  --command-name='Bun server, Bun client' --prepare='cd website-bunbuns && bun install && ../start-server.sh bun --bun run run-server-worker.mjs' 'cd website-bunbuns && bun run tools/check-links.mjs http://localhost:9001/' \
-  --command-name='Node.js server, Node.js client' --prepare='cd website-node-20 && yarn install && ../start-server.sh node run-server-worker.mjs' 'cd website-node-20 && node tools/check-links.mjs http://localhost:9001/' \
+  --command-name='Bun server (Bun APIs), Bun client' --prepare='cd website-bunbuns && ../start-server.sh bun --bun run run-server-worker.mjs' 'cd website-bunbuns && bun --bun run tools/check-links.mjs http://localhost:9001/' \
+  --command-name='Bun server (Bun APIs), Node.js client (fetch)' --prepare='cd website-bunbuns && ../start-server.sh bun --bun run run-server-worker.mjs' 'cd website-node-20 && node tools/check-links.mjs http://localhost:9001/' \
+  --command-name='Bun server (Bun APIs), Node.js client (node:http)' --prepare='cd website-bunbuns && ../start-server.sh bun --bun run run-server-worker.mjs' 'cd website-node-14 && node tools/check-links.mjs http://localhost:9001/' \
+  --command-name='Bun server (Node.js APIs), Bun client' --prepare='cd website-bunnode && ../start-server.sh bun --bun run run-server-worker.mjs' 'cd website-bunbuns && bun --bun run tools/check-links.mjs http://localhost:9001/' \
+  --command-name='Bun server (Node.js APIs), Node.js client (fetch)' --prepare='cd website-bunnode && ../start-server.sh bun --bun run run-server-worker.mjs' 'cd website-node-20 && node tools/check-links.mjs http://localhost:9001/' \
+  --command-name='Bun server (Node.js APIs), Node.js client (node:http)' --prepare='cd website-bunnode && ../start-server.sh bun --bun run run-server-worker.mjs' 'cd website-node-14 && node tools/check-links.mjs http://localhost:9001/' \
+  --command-name='Node.js server, Bun client' --prepare='cd website-node-20 && ../start-server.sh node run-server-worker.mjs' 'cd website-bunbuns && bun --bun run tools/check-links.mjs http://localhost:9001/' \
+  --command-name='Node.js server, Node.js client (fetch)' --prepare='cd website-node-20 && ../start-server.sh node run-server-worker.mjs' 'cd website-node-20 && node tools/check-links.mjs http://localhost:9001/' \
+  --command-name='Node.js server, Node.js client (node:http)' --prepare='cd website-node-20 && ../start-server.sh node run-server-worker.mjs' 'cd website-node-14 && node tools/check-links.mjs http://localhost:9001/' \
