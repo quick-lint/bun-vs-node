e85246d4b4773b361d7cf7dd79c2480312f22764
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-09-11 20:54:18 -0700

    Add dev server benchmarks

---
+++ benchmark-serve-hot.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+set -u
+set -o pipefail
+
+hyperfine \
+  --export-json=benchmark-serve-hot.json \
+  --warmup=1 \
+  --time-unit=millisecond \
+  --prepare='cd website-bunbuns && bun install && ../start-server.sh bun run run-server-worker.mjs' 'cd website-bunbuns && bun run tools/check-links.mjs http://localhost:9001/' \
+  --prepare='cd website-node-20 && yarn install && ../start-server.sh node run-server-worker.mjs' 'cd website-node-20 && node tools/check-links.mjs http://localhost:9001/' \
---
+++ start-server.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+set -u
+set -o pipefail
+
+here="$(dirname "${0}")"
+
+wait_for_server_start() {
+  while ! curl --silent -o /dev/null http://localhost:9001/ ; do
+    sleep 0.1
+  done
+}
+
+if [ -f "${here}/server.pid" ]; then
+  kill -TERM -"$(cat "${here}/server.pid")" || :
+fi
+
+if lsof -i :9001 ; then
+  echo "error: server already running but not managed by us" >&2
+  exit 1
+fi
+
+setsid "${@}" >"${here}/server.log" 2>&1 &
+echo "${!}" >"${here}/server.pid"
+wait_for_server_start
