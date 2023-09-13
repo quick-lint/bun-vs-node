b7c4b2f32e280c5834235a35db9b63e7ca5a3cb5
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-09-12 23:03:20 -0700

    Control filesystem caches for warm package install test
    Simulate CI better.

--- benchmark-package-install-ci-warm.sh
+++ benchmark-package-install-ci-warm.sh
@@ -3,16 +3,20 @@ set -e
 set -u
 set -o pipefail
 
+clear_fs_cache='echo 3 | sudo tee /proc/sys/vm/drop_caches'
+warm_yarn_package_cache='find ~/.cache/yarn/ -type f -exec cat "{}" \+ >/dev/null'
+warm_bun_package_cache='find ~/.bun/install/cache/ -type f -exec cat "{}" \+ >/dev/null'
+
 hyperfine \
   --export-json=benchmark-package-install-ci-warm-website.json \
   --warmup=2 \
   --time-unit=millisecond \
-  --command-name='Yarn (website)' --prepare='rm -rf website-node-20/node_modules/' 'cd website-node-20 && yarn install --immutable' \
-  --command-name='Bun (website)' --prepare='rm -rf website-bunbuns/node_modules/' 'cd website-bunbuns && bun install --frozen-lockfile' \
+  --command-name='Yarn (website)' --prepare="rm -rf website-node-20/node_modules/ && ${clear_fs_cache} && ${warm_yarn_package_cache}" 'cd website-node-20 && yarn install --immutable' \
+  --command-name='Bun (website)' --prepare="rm -rf website-bunbuns/node_modules/ && ${clear_fs_cache} && ${warm_bun_package_cache}" 'cd website-bunbuns && bun install --frozen-lockfile' \
 
 hyperfine \
   --export-json=benchmark-package-install-ci-warm-analytics.json \
   --warmup=2 \
   --time-unit=millisecond \
-  --command-name='Yarn (analytics)' --prepare='rm -rf website-node-20/analytics/node_modules/' 'cd website-node-20/analytics && yarn install --immutable' \
-  --command-name='Bun (analytics)' --prepare='rm -rf website-bunbuns/analytics/node_modules/' 'cd website-bunbuns/analytics && bun install --frozen-lockfile' \
+  --command-name='Yarn (analytics)' --prepare="rm -rf website-node-20/analytics/node_modules/ && ${clear_fs_cache} && ${warm_yarn_package_cache}" 'cd website-node-20/analytics && yarn install --immutable' \
+  --command-name='Bun (analytics)' --prepare="rm -rf website-bunbuns/analytics/node_modules/ && ${clear_fs_cache} && ${warm_bun_package_cache}" 'cd website-bunbuns/analytics && bun install --frozen-lockfile' \
