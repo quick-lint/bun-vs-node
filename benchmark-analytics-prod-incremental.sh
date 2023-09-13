f5ca9314fde7432f8b20339b8380bb15203606af
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-09-12 19:34:13 -0700

    Use newer Bun in analytics benchmark

--- benchmark-analytics-prod-incremental.sh
+++ benchmark-analytics-prod-incremental.sh
@@ -7,7 +7,7 @@ ssh qljs-analytics@c.quick-lint-js.com '
   cp analytics.sqlite3 analytics-bun-vs-node-node-20.sqlite3
   cp analytics.sqlite3 analytics-bun-vs-node-bunbuns.sqlite3
 
-  PATH="${HOME}/bun-linux-x64-baseline:${PATH}"
+  PATH="${HOME}/bun-linux-x64-1.0.1:${PATH}"
 
   hyperfine \
     --export-json=benchmark-analytics-prod-incremental-import-apache-logs.json \
