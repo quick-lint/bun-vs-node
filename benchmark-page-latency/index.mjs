5cbf49afc52fce4e7541aa02888a1325908edcd9
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-09-13 16:41:31 -0700

    benchmark-page-latency: log memory usage

--- benchmark-page-latency/index.mjs
+++ benchmark-page-latency/index.mjs
@@ -1,4 +1,9 @@
+import child_process from 'node:child_process';
+import fs from 'node:fs';
 import puppeteer from 'puppeteer';
+import util from 'node:util';
+
+let execFileAsync = util.promisify(child_process.execFile);
 
 let pagesToTest = [
   // some SVG, some JS
@@ -20,25 +25,66 @@ let pagesToTest = [
   'http://localhost:9001/merch/',
 ];
 
+let running = false;
+
 async function mainAsync() {
   const browser = await puppeteer.launch({headless: 'new'});
-  const page = await browser.newPage();
+  try {
+    const page = await browser.newPage();
+    await page.setViewport({width: 1920, height: 1080});
 
-  await page.setViewport({width: 1920, height: 1080});
+    let pids = await getServerProcessIDsAsync();
+    running = true;
+    await Promise.all([benchmarkAsync(page), logMemoryUsageAsync(pids)]);
+  } finally {
+    await browser.close();
+  }
+}
 
-  for (let i = 0; i < 30; ++i) {
-    for (let url of pagesToTest) {
-      await sleepAsync(100);
-      let before = performance.now();
-      await page.goto(url);
-      let after = performance.now();
-      if (i >= 3) {
-        console.log(`${url},${after - before}`);
+async function getServerProcessIDsAsync() {
+  let mainPID = parseInt(fs.readFileSync('../server.pid'), 10);
+  let {stdout, stderr} = await execFileAsync('pstree', ['--show-pids', '--hide-threads', '--ascii', mainPID]);
+  return [...stdout.matchAll(/\((\d+)\)/g)].map(match => parseInt(match[1], 10));
+}
+
+async function benchmarkAsync(page) {
+  try {
+    for (let i = 0; i < 30; ++i) {
+      for (let url of pagesToTest) {
+        await sleepAsync(100);
+        let before = performance.now();
+        await page.goto(url);
+        let after = performance.now();
+        if (i >= 3) {
+          console.log(`load,${url},${after - before}`);
+        }
       }
     }
+  } finally {
+    running = false;
   }
+}
 
-  await browser.close();
+async function logMemoryUsageAsync(pids) {
+  while (running) {
+    console.log(`mem,${performance.now()},${getMemoryUsageForProcesses(pids)}`);
+    await sleepAsync(100);
+  }
+}
+
+function getMemoryUsageForProcesses(pids) {
+  return pids.reduce((acc, pid) => acc + getMemoryUsageForProcess(pid), 0);
+}
+
+function getMemoryUsageForProcess(pid) {
+  let smapsLines = fs.readFileSync(`/proc/${pid}/smaps_rollup`, 'utf-8').split('\n');
+  for (let line of smapsLines) {
+    if (line.startsWith("Rss:")) {
+      let kbText = line.match(/\d+/)[0];
+      return parseInt(kbText, 10) * 1024;
+    }
+  }
+  throw new Error(`failed to get memory usage for process ${pid}`);
 }
 
 function sleepAsync(milliseconds) {
