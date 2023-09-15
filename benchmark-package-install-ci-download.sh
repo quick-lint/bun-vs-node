fb587d0edab889f8b372644c4f34d1090f0ca91a
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-09-14 23:56:25 -0700

    Benchmark yarnpkg.com vs npmjs.org for 'yarn install'

--- benchmark-package-install-ci-download.sh
+++ benchmark-package-install-ci-download.sh
@@ -7,12 +7,14 @@ hyperfine \
   --export-json=benchmark-package-install-ci-download-website.json \
   --warmup=1 \
   --time-unit=millisecond \
-  --command-name='Yarn (website)' --prepare='rm -rf ~/.cache/yarn/ ~/Library/Caches/Yarn/v6/ website-node-20/node_modules/' 'cd website-node-20 && yarn install --immutable' \
-  --command-name='Bun (website)' --prepare='rm -rf ~/.bun/install/cache/ website-bunbuns/node_modules/' 'cd website-bunbuns && bun install --frozen-lockfile' \
+  --command-name='Yarn (website) (yarnpkg.com)' --prepare='rm -rf ~/.cache/yarn/ ~/Library/Caches/Yarn/v6/ website-node-20/node_modules/' 'cd website-node-20 && yarn install --immutable' \
+  --command-name='Yarn (website) (npmjs.org)' --prepare='rm -rf ~/.cache/yarn/ ~/Library/Caches/Yarn/v6/ website-npmjsor/node_modules/' 'cd website-npmjsor && yarn install --immutable' \
+  --command-name='Bun (website) (npmjs.org)' --prepare='rm -rf ~/.bun/install/cache/ website-bunbuns/node_modules/' 'cd website-bunbuns && bun install --frozen-lockfile' \
 
 hyperfine \
   --export-json=benchmark-package-install-ci-download-analytics.json \
   --warmup=1 \
   --time-unit=millisecond \
-  --command-name='Yarn (analytics)' --prepare='rm -rf ~/.cache/yarn/ ~/Library/Caches/Yarn/v6/ website-node-20/analytics/node_modules/' 'cd website-node-20/analytics && yarn install --immutable' \
-  --command-name='Bun (analytics)' --prepare='rm -rf ~/.bun/install/cache/ website-bunbuns/analytics/node_modules/' 'cd website-bunbuns/analytics && bun install --frozen-lockfile' \
+  --command-name='Yarn (analytics) (yarnpkg.com)' --prepare='rm -rf ~/.cache/yarn/ ~/Library/Caches/Yarn/v6/ website-node-20/analytics/node_modules/' 'cd website-node-20/analytics && yarn install --immutable' \
+  --command-name='Yarn (analytics) (npmjs.org)' --prepare='rm -rf ~/.cache/yarn/ ~/Library/Caches/Yarn/v6/ website-npmjsor/analytics/node_modules/' 'cd website-npmjsor/analytics && yarn install --immutable' \
+  --command-name='Bun (analytics) (npmjs.org)' --prepare='rm -rf ~/.bun/install/cache/ website-bunbuns/analytics/node_modules/' 'cd website-bunbuns/analytics && bun install --frozen-lockfile' \
